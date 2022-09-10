b5f9aa21f3a52bd55e667b0d2b09edf24a2e75f6
Nati <fabbro.natalia@gmail.com>
2022-09-10 12:23:11 -0300

    Cambios para deploy

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Game from './pages/game';
 import 'bulma/css/bulma.min.css';
+import packageInfo from '../package.json';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={getBasename()}>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/game' element={<Game/>}/>
@@ -14,5 +15,10 @@ function App() {
     </BrowserRouter>
   );
 }
+
+function getBasename(){
+  const array = packageInfo.homepage.split("/");
+  return array[array.length-1];
+} 
 
 export default App;
